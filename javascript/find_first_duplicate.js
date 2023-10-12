5cf6b81400a8d02cb974966c5b4c9b71e7ba8a91
Noah <noahhughesph@gmail.com>
2023-10-11 21:09:18 -0400

    done

--- javascript/find_first_duplicate.js
+++ javascript/find_first_duplicate.js
@@ -1,16 +1,23 @@
 function findFirstDuplicate(arr) {
-  // type your code here
+  const seen = new Set();
+  for (const curr of arr) {
+    if (seen.has(curr)) {
+      return curr;
+    }
+    seen.add(curr);
+  }
+  return -1;
 }
 
 if (require.main === module) {
   // add your own tests in here
-  console.log("Expecting: 3");
-  console.log("=>", findFirstDuplicate([2, 1, 3, 3, 2]));
+  console.log('Expecting: 3');
+  console.log('=>', findFirstDuplicate([2, 1, 3, 3, 2]));
 
-  console.log("");
+  console.log('');
 
-  console.log("Expecting: -1");
-  console.log("=>", findFirstDuplicate([1, 2, 3, 4]));
+  console.log('Expecting: -1');
+  console.log('=>', findFirstDuplicate([1, 2, 3, 4]));
 }
 
 module.exports = findFirstDuplicate;
